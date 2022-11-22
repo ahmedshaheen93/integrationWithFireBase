c8c96417e3567d827fea29ea69a0010cd64e546d
ahmed shaheen <ahmedshaheen93@yahoo.com>
2022-11-22 21:07:37 +0200

    init the project

---
+++ src/main/java/com/shaheen/integrationwithfirebase/IntegrationWithFireBaseApplication.java
@@ -0,0 +1,13 @@
+package com.shaheen.integrationwithfirebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationWithFireBaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IntegrationWithFireBaseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/shaheen/integrationwithfirebase/IntegrationWithFireBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaheen.integrationwithfirebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationWithFireBaseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
